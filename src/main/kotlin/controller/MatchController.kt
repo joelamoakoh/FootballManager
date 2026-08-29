c2e26a1fbe6f5536525901dafe5ab5b51ae79dfd
Joel Amoakoh <20096482@mail.wit.ie>
2026-08-29 22:35:21 +0100

    add matchcontroller and kdoc

--- src/main/kotlin/controller/MatchController.kt
+++ src/main/kotlin/controller/MatchController.kt
@@ -2,23 +2,43 @@ package controller
 
 import model.Match
 import java.util.ArrayList
-
+/**
+ * Controls and manages the collection of matches in the application
+ */
 class MatchController {
 
     private val matches = ArrayList<Match>()
-
+    /**
+     * Adds a match to collection
+     *
+     * @param match the match to add
+     */
     fun addMatch(match: Match) {
         matches.add(match)
     }
-
+    /**
+     * Returns all matches that are stored
+     *
+     * @return the list of  matches stored
+     */
     fun getMatches(): ArrayList<Match> {
         return matches
     }
+    /**
+     * Removes a match from the collection
+     *
+     * @param match the player to remove
+     */
 
     fun removeMatch(match: Match) {
         matches.remove(match)
     }
-
+    /**
+     * Finds a match its index in collection
+     *
+     * @param index the index of the match to find
+     * @return match with that index or null if index is not used
+     */
     fun findMatch(index: Int): Match? {
         return if (index >= 0 && index < matches.size) {
             matches[index]
@@ -26,6 +46,14 @@ class MatchController {
             null
         }
     }
+    /**
+     * Updates the details of a match
+     *
+     * @param indexToUpdate the index of the match you want to update
+     * @param match the new match details
+     * @return true if the match was updated successfully, false if not
+     */
+
 
     fun updateMatch(indexToUpdate: Int, match: Match?): Boolean {
         val foundMatch = findMatch(indexToUpdate)
@@ -41,7 +69,12 @@ class MatchController {
 
         return false
     }
-
+    /**
+     * Finds a match by its opponent
+     *
+     * @param opponent the opponent to search for
+     * @return the matching match null if theres no match
+     */
     fun findMatch(opponent: String): Match? {
         return matches.find {
             it.opponent.equals(opponent, ignoreCase = true)
